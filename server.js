493b73d9af637fec4c60cf66a2e2f16413b12703
Glitch (hello-express) <none>
2019-11-10 22:22:47 +0000

    🎙🌏 Checkpoint ./server.js:2245715/500 ./views/index.html:2245715/6

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const fs = require('fs');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+
+const fs = require('fs');
+
+
+
+
 
 app.use(express.static("public"));
 
@@ -36,13 +42,14 @@ const loadData = (path) => {
 
 
 io.on('connection', function(socket){
-
+console.log("connected");
 socket.on('new-net-space',function(netSpace){ // Listen for new-player event on this client 
   //  socket.broadcast.emit('text-changed',"howdy");
       //get old record.json
   //update with new data
   //storeData("hi,78","/app/record.json");
   console.log("new net space = "+netSpace);
+  console.log("hello");
     })
 
-});
+})
