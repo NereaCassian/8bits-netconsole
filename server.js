e93ec74dd8590fb3f55807e69382dc5b18657774
Glitch (netrunning) <none>
2019-11-22 16:28:55 +0000

    🚁🍯 Checkpoint ./server.js:2245715/161

--- server.js
+++ server.js
@@ -62,7 +62,10 @@ io.on("connection", function(socket) {
   socket.on("get-current-netspace-names",function(){
     var data = JSON.parse(loadData(record));
     var keys=data.keys();
-    for()
+//     for(var i = 0; i<keys.length;i++){
+      socket.emit("key-names", keys);
+//     }
+   
     
   });
  
