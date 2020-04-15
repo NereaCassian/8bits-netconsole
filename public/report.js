53ce9f367067b29c96bbdf3c5f931b2d176f214f
Glitch (netrunning) <none>
2020-04-15 16:45:25 +0000

    💧✍️ Checkpoint ./public/report.js:2245715/544

--- public/report.js
+++ public/report.js
@@ -20,4 +20,23 @@ socket.emit("get-report",queryString);
 
 socket.on("load-report", function(report,name) {
   console.log("report = "+report+"name = "+name);
+  
+  for(var i=0;i<report.length;i++){
+    var user;
+    if(report[i][1]=="userText") user = true;
+    else user = false;
+    addLogText(report[i][0], user);
+  }
+  
 });
+
+
+function addLogText(text, user, damage) {
+  var userText = document.createElement("P");
+  userText.innerHTML = text;
+  if (user) userText.className = "userText";
+  else if (damage) userText.className = "damageText";
+  var log = document.getElementById("log");
+  log.appendChild(userText);
+  // if(!user) window.scrollTo(0,document.body.scrollHeight);
+}
